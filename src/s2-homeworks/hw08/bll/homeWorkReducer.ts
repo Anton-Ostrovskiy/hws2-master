f6796d85983daa0761782b745c25a7cf2a47fbe7
Anton <ostrovskiy.98@mail.ru>
2025-09-07 18:08:08 +0300

    completed hw8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,17 +4,21 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => {
+  // need to fix any
+  switch (action.type) {
+    case "sort": {
+      // by name
 
-            return state // need to fix
-        }
-        case 'check': {
-
-            return state // need to fix
-        }
-        default:
-            return state
+       return [...state].sort((a, b) => {
+         const comparison = a.name.localeCompare(b.name, "ru");
+         return action.payload === "up" ? comparison : -comparison;
+       });
     }
-}
+    case "check": {
+      return state.filter((el) => el.age >= 18); // need to fix
+    }
+    default:
+      return state;
+  }
+};
